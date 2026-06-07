0e47de067ca48eca7bd09a689ebed2fbef57ba21
elidbear2010s <elijahcastrono8910@gmail.comProfile image elijahcastrono8910@gmail.com>
2026-06-07 15:01:17 -0500

    set it all up

--- src/main/java/net/elidbear2010/simpleweaponscreator/SimpleWeaponsCreator.java
+++ src/main/java/net/elidbear2010/simpleweaponscreator/SimpleWeaponsCreator.java
@@ -1,5 +1,7 @@
 package net.elidbear2010.simpleweaponscreator;
 
+import net.elidbear2010.simpleweaponscreator.items.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -29,6 +31,8 @@ public class SimpleWeaponsCreator {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
 
@@ -42,6 +46,10 @@ public class SimpleWeaponsCreator {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
+
+        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+            event.accept(ModItems.CRYSTALS);
+        }
 
     }
 
---
+++ src/main/java/net/elidbear2010/simpleweaponscreator/items/ModItems.java
@@ -0,0 +1,19 @@
+package net.elidbear2010.simpleweaponscreator.items;
+
+import net.elidbear2010.simpleweaponscreator.SimpleWeaponsCreator;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(SimpleWeaponsCreator.MOD_ID);
+
+    public static final DeferredItem<Item> CRYSTALS = ITEMS.register("crystal",
+            () -> new Item(new Item.Properties()));
+
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
