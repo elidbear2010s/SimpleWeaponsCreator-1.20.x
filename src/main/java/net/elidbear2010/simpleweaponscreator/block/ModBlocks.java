7995b338d20584be338dc10671cbc8f6b272df75
elidbear2010s <elijahcastrono8910@gmail.comProfile image elijahcastrono8910@gmail.com>
2026-06-08 15:24:37 -0500

    new changes

--- src/main/java/net/elidbear2010/simpleweaponscreator/block/ModBlocks.java
+++ src/main/java/net/elidbear2010/simpleweaponscreator/block/ModBlocks.java
@@ -5,6 +5,8 @@ import net.elidbear2010.simpleweaponscreator.items.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -15,7 +17,9 @@ public class ModBlocks {
     static public final DeferredRegister.Blocks BLOCKS =
             DeferredRegister.createBlocks(SimpleWeaponsCreator.MOD_ID);
 
-
+    public static final DeferredBlock<Block> ROUGHCRYSTAL_BLOCK = registerBlock("roughcrystal_block",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.AMETHYST)));
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
